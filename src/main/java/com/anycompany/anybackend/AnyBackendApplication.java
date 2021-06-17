07b1b063c1da576186c44c4af4badfbc42238b04
birnu <60793624+birnu@users.noreply.github.com>
2021-06-17 14:04:20 +0200

    this is commitment

---
+++ src/main/java/com/anycompany/anybackend/AnyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.anycompany.anybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anycompany/anybackend/AnyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anycompany.anybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

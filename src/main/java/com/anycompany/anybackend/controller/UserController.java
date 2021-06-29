57a03631235936f41b6bb964b5f33701c7c3300d
OreoCassowary <arthurkaplan@gmx.at>
2021-06-29 10:36:04 +0200

    UserController: changed /users to /user

--- src/main/java/com/anycompany/anybackend/controller/UserController.java
+++ src/main/java/com/anycompany/anybackend/controller/UserController.java
@@ -7,31 +7,31 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class UserController {
 
-    @GetMapping("/users")
+    @GetMapping("/user")
     public User getUsers() {
         // todo: return list of users as JSON
         return null;
     }
 
-    @PostMapping("/users/")
+    @PostMapping("/user/")
     public User createUser(@RequestBody User user) {
         // todo: adds a new user to a list of users
         return null;
     }
 
-    @GetMapping("/users/{userId}")
+    @GetMapping("/user/{userId}")
     public User getUserInfo(@PathVariable int userId) {
         // todo: get info for a specific user
         return null;
     }
 
-    @PutMapping("/users/{userId}")
+    @PutMapping("/user/{userId}")
     public User changeUserInfo(@PathVariable int userId) {
         // todo: change attributes of a specific user
         return null;
     }
 
-    @DeleteMapping("/users/{userId}")
+    @DeleteMapping("/user/{userId}")
     public User removeUser(@PathVariable int userId){
         // todo: delete specific user from database
         return null;

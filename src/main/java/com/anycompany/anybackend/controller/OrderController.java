e88dc872baf82d302b0c9b070eb2bf30d3c699f9
OreoCassowary <arthurkaplan@gmx.at>
2021-11-19 10:34:59 +0100

    Populated OrderController.java with placeholder methods (return null)

--- src/main/java/com/anycompany/anybackend/controller/OrderController.java
+++ src/main/java/com/anycompany/anybackend/controller/OrderController.java
@@ -1,5 +1,38 @@
 package com.anycompany.anybackend.controller;
 
+import com.anycompany.anybackend.model.Order;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class OrderController {
-    // todo
+
+    @GetMapping("/allOrders")
+    public Order getAllOrders() {
+        // todo: show all orders (admin view)
+        return null;
+    }
+
+    @GetMapping("/user/{userId}/order")
+    public Order showSingleOder() {
+        // todo: show order user's order
+        return null;
+    }
+
+    @PostMapping("/user/{userId}/cart")
+    public Order createOrder() {
+        return null;
+    }
+
+    @PutMapping("/user/{userId}/cart")
+    public Order updateOrder() {
+        return null;
+    }
+
+    @DeleteMapping("/user/{userId}/cart")
+    public Order cancelOrder() {
+        return null;
+    }
+
+
+
 }
